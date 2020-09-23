a1fca296650d9e487861f15e38a84d65fd74d4a1
Wighawag <wighawag@gmail.com>
2020-09-23 09:57:19 +0100

    use latest named-logs

--- dist/cjs/errors.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.CHAIN_ID_NOT_SET = exports.CHAIN_ID_FAILED = exports.MODULE_ERROR = exports.CHAIN_CONFIG_NOT_AVAILABLE = exports.CHAIN_NO_PROVIDER = void 0;
-exports.CHAIN_NO_PROVIDER = 6000;
-exports.CHAIN_CONFIG_NOT_AVAILABLE = 6001;
-exports.MODULE_ERROR = 1000;
-exports.CHAIN_ID_FAILED = 2001;
-exports.CHAIN_ID_NOT_SET = 2002;
--- dist/cjs/utils/builtin.d.ts
+++
@@ -1,6 +0,0 @@
-declare type Ethereum = any;
-export declare function getEthereum(): Ethereum;
-export declare function fetchEthereum(): Promise<Ethereum>;
-export declare function getVendor(ethereum: Ethereum): string | undefined;
-export {};
-//# sourceMappingURL=builtin.d.ts.map
